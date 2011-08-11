1ff5725e4e5908196fc1bce25be8ca52cab2380a
nov matake <nov@matake.jp>
2011-08-11 16:31:08 +0900

    rack-oauth2 response type extensions for "id_token" and "id_token token"

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -1,3 +1,7 @@
+require 'rack/oauth2'
+require 'rack/oauth2/server/authorize/extensions/id_token'
+require 'rack/oauth2/server/authorize/extensions/id_token_and_token'
+
 module OpenIDConnect
   # Your code goes here...
 end
---
+++ lib/rack/oauth2/server/authorize/extensions/id_token_and_token.rb
@@ -0,0 +1,34 @@
+module Rack
+  module OAuth2
+    module Server
+      class Authorize
+        module Extensions
+          class IdTokenAndToken < Abstract::Handler
+            class << self
+              def response_type_for?(response_type)
+                response_type.split.sort == ['id_token', 'token']
+              end
+            end
+
+            def call(env)
+              @request  = Request.new env
+              @response = Response.new request
+              super
+            end
+
+            class Request < Authorize::Token::Request
+              def initialize(env)
+                super
+                @response_type = [:id_token, :token]
+                attr_missing!
+              end
+            end
+
+            class Response < Authorize::Token::Response
+            end
+          end
+        end
+      end
+    end
+  end
+end
