3199ff25694a2e445084bf29c994bc0ce0642fe9
nov matake <nov@matake.jp>
2011-08-17 21:49:02 +0900

    remove unnecessary require

--- lib/openid_connect/client.rb
+++ lib/openid_connect/client.rb
@@ -1,6 +1,3 @@
-require 'rack/oauth2/client/error'
-require 'rack/oauth2/client/grant'
-
 module OpenIDConnect
   class Client < Rack::OAuth2::Client
     attr_optional :introspection_endpoint, :user_info_endpoint
