5de38896d3309651baf3046bfc4306361f036847
nov matake <nov@matake.jp>
2011-08-17 16:09:55 +0900

    rename etc

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'rack/oauth2'
-require 'rack/oauth2/server/authorize/token_ext'
+require 'rack/oauth2/server/authorize/token_with_id_token'
 
 require 'openid_connect/exception'
 require 'openid_connect/client'
