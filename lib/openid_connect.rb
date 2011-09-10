57ccd790d07b91ebe68836901fea3f35e7ad2181
nov matake <nov@matake.jp>
2011-09-10 18:54:54 +0900

    v0.0.18 debugger

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -1,6 +1,39 @@
 require 'json'
+require 'logger'
 require 'rack/oauth2'
 require 'rack/oauth2/server/id_token_response'
+
+module OpenIDConnect
+  def self.logger
+    @@logger
+  end
+  def self.logger=(logger)
+    @@logger = logger
+  end
+  self.logger = Logger.new(STDOUT)
+  self.logger.progname = 'OpenIDConnect'
+
+  def self.debugging?
+    @@debugging
+  end
+  def self.debugging=(boolean)
+    @@debugging = boolean
+  end
+  def self.debug!
+    Rack::OAuth2.debugging = true
+    self.debugging = true
+  end
+  def self.debug(&block)
+    rack_oauth2_original = Rack::OAuth2.debugging?
+    original = self.debugging?
+    debug!
+    yield
+  ensure
+    Rack::OAuth2.debugging = rack_oauth2_original
+    self.debugging = original
+  end
+  self.debugging = false
+end
 
 require 'openid_connect/exception'
 require 'openid_connect/client'
