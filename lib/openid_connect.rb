b5e50f9f5967d00ca2e242b75df34fa57961eceb
nov matake <nov@matake.jp>
2012-02-18 00:55:21 +0900

    make RequestWithConnectParams module

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -8,7 +8,7 @@ require 'validate_email'
 require 'attr_required'
 require 'attr_optional'
 require 'rack/oauth2'
-require 'rack/oauth2/server/authorize'
+require 'rack/oauth2/server/authorize/request_with_connect_params'
 require 'rack/oauth2/server/id_token_response'
 
 module OpenIDConnect
