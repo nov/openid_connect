654b51282a8ae97d9f43a0acbbdaca3c2c212296
nov matake <nov@matake.jp>
2012-02-18 00:55:37 +0900

    missing file

---
+++ lib/rack/oauth2/server/authorize/request_with_connect_params.rb
@@ -0,0 +1,20 @@
+module Rack::OAuth2::Server::Authorize
+  module RequestWithConnectParams
+    # NOTE: nonce is REQUIRED, but define optional attributes not to break rack-oauth2 for now
+    CONNECT_EXT_PARAMS = [:nonce, :display, :prompt, :request, :request_uri]
+
+    def self.included(klass)
+      klass.send :attr_optional, *CONNECT_EXT_PARAMS
+      klass.class_eval do
+        def initialize_with_connect_params(env)
+          initialize_without_nonce env
+          CONNECT_EXT_PARAMS.each do |attribute|
+            self.send :"#{attribute}=", params[attribute.to_s]
+          end
+        end
+        alias_method_chain :initialize, :connect_params
+      end
+    end
+  end
+  Request.send :include, RequestWithConnectParams
+end
