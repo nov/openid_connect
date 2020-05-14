c41009acba332943b25128af3f1887fb2ba01a5e
Nahum Cohen <nahum.cohen@cyberark.com>
2020-05-14 12:38:35 +0300

    Use SHA256 instead of MD5, which allow code to be FIPS compliant

--- spec/openid_connect/discovery/provider/config/resource_spec.rb
+++ spec/openid_connect/discovery/provider/config/resource_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe OpenIDConnect::Discovery::Provider::Config::Resource do
   let(:resource) do
     uri = URI.parse 'http://server.example.com'
+#    SHA256 = 'e1335e881cf563c3fe4fb26a39fb410012e940a52cd9fa456fbe37b48d213ba2'
     OpenIDConnect::Discovery::Provider::Config::Resource.new uri
   end
 
@@ -16,5 +17,12 @@ describe OpenIDConnect::Discovery::Provider::Config::Resource do
         expect { resource.endpoint }.to raise_error SWD::Exception
       end
     end
+
+#   since the cache_key is private can't test it
+#    context 'when validate sha256 on host' do
+#      it do
+#        resource.cache_key.should == "swd:resource:opneid-conf:#{SHA256}"
+#      end
+#    end
   end
 end
